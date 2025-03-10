2079ea9d99fbc8fa5c3730fa8349f64754d4d84d
Diego Bastidas <diegofernando150@gmail.com>
2025-03-10 10:15:52 -0500

    Base de datos: Entidades de usuario y CV

--- src/Entity/CurriculumVitae.php
+++ src/Entity/CurriculumVitae.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CurriculumVitaeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CurriculumVitaeRepository::class)]
@@ -17,6 +19,24 @@ class CurriculumVitae
     #[ORM\JoinColumn(nullable: false)]
     private ?User $owner = null;
 
+    /**
+     * @var Collection<int, Education>
+     */
+    #[ORM\OneToMany(targetEntity: Education::class, mappedBy: 'cv', orphanRemoval: true)]
+    private Collection $education;
+
+    /**
+     * @var Collection<int, Experience>
+     */
+    #[ORM\OneToMany(targetEntity: Experience::class, mappedBy: 'cv', orphanRemoval: true)]
+    private Collection $experiences;
+
+    public function __construct()
+    {
+        $this->education = new ArrayCollection();
+        $this->experiences = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -30,6 +50,66 @@ class CurriculumVitae
     public function setOwner(?User $owner): static
     {
         $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Education>
+     */
+    public function getEducation(): Collection
+    {
+        return $this->education;
+    }
+
+    public function addEducation(Education $education): static
+    {
+        if (!$this->education->contains($education)) {
+            $this->education->add($education);
+            $education->setCv($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEducation(Education $education): static
+    {
+        if ($this->education->removeElement($education)) {
+            // set the owning side to null (unless already changed)
+            if ($education->getCv() === $this) {
+                $education->setCv(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Experience>
+     */
+    public function getExperiences(): Collection
+    {
+        return $this->experiences;
+    }
+
+    public function addExperience(Experience $experience): static
+    {
+        if (!$this->experiences->contains($experience)) {
+            $this->experiences->add($experience);
+            $experience->setCv($this);
+        }
+
+        return $this;
+    }
+
+    public function removeExperience(Experience $experience): static
+    {
+        if ($this->experiences->removeElement($experience)) {
+            // set the owning side to null (unless already changed)
+            if ($experience->getCv() === $this) {
+                $experience->setCv(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Education.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EducationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EducationRepository::class)]
+class Education
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $degree = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $start_date = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $end_date = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $alma_mater = null;
+
+    #[ORM\ManyToOne(inversedBy: 'education')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?CurriculumVitae $cv = null;
+
+    #[ORM\Column]
+    private ?int $user_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDegree(): ?string
+    {
+        return $this->degree;
+    }
+
+    public function setDegree(string $degree): static
+    {
+        $this->degree = $degree;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->start_date;
+    }
+
+    public function setStartDate(?\DateTimeInterface $start_date): static
+    {
+        $this->start_date = $start_date;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->end_date;
+    }
+
+    public function setEndDate(?\DateTimeInterface $end_date): static
+    {
+        $this->end_date = $end_date;
+
+        return $this;
+    }
+
+    public function getAlmaMater(): ?string
+    {
+        return $this->alma_mater;
+    }
+
+    public function setAlmaMater(?string $alma_mater): static
+    {
+        $this->alma_mater = $alma_mater;
+
+        return $this;
+    }
+
+    public function getCv(): ?CurriculumVitae
+    {
+        return $this->cv;
+    }
+
+    public function setCv(?CurriculumVitae $cv): static
+    {
+        $this->cv = $cv;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
