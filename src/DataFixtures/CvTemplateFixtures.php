ebd8383d461e80dadb1deed9fda30cd42064abda
Diego Bastidas <diegofernando150@gmail.com>
2025-03-21 15:40:19 -0500

    nuevas plantillas

--- src/DataFixtures/CvTemplateFixtures.php
+++ src/DataFixtures/CvTemplateFixtures.php
@@ -29,9 +29,27 @@ class CvTemplateFixtures extends Fixture {
         $grayIsLife->setPageWidth('180mm');
         $grayIsLife->setPageHeight('235mm');
 
+        $jumpYourCV = new CvTemplate();
+        $jumpYourCV->setThumbnail('Captura-de-Pantalla-2025-03-17-a-la-s-8-03-08-p-m-67d8c692e8a44.png');
+        $jumpYourCV->setName('Jump Your CV');
+        $jumpYourCV->setHtml(file_get_contents(__DIR__ . '/../../templates/fixtures/jump_your_cv.html.twig'));
+        $jumpYourCV->setPageWidth('180mm');
+        $jumpYourCV->setPageHeight('235mm');
+
+
+        $beatyWhite = new CvTemplate();
+        $beatyWhite->setThumbnail('Captura-de-Pantalla-2025-03-21-a-la-s-3-23-49-p-m-67ddcb0576037.png');
+        $beatyWhite->setName('Beauty White');
+        $beatyWhite->setHtml(file_get_contents(__DIR__ . '/../../templates/fixtures/beauty_white.html.twig'));
+        $beatyWhite->setPageWidth('160mm');
+        $beatyWhite->setPageHeight('227mm');
+
+
         $manager->persist($hollyDommy);
         $manager->persist($hollyMommy);
         $manager->persist($grayIsLife);
+        $manager->persist($beatyWhite);
+        $manager->persist($jumpYourCV);
         $manager->flush();
     }
 }
